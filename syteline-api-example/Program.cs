cd2477052beb155c1c447fa70a59f9dd083f6750
Andy Mercer <61432947+amercer-fdi@users.noreply.github.com>
2025-10-22 16:58:45 -0400

    Update Program.cs

--- syteline-api-example/Program.cs
+++ syteline-api-example/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace syteline_api_example {
+﻿using syteline_api_example.Helpers;
+using syteline_api_example.Models;
+
+namespace syteline_api_example {
 
     internal class Program {
 
@@ -6,42 +9,48 @@
 
             // INITIALIZE THE RESTv2 API THROUGH ION, USING THE CREDENTIALS THAT YOU DOWNLOAD AFTER CREATING AN AUTHORIZED APP AND SERVICE ACCOUNT
 
-            SytelineRestAPI sytelineRestAPIThroughION = new(
-                IONAPICredentials: new IONAuthManager(
-                    ti: "",
-                    cn: "",
-                    dt: "",
-                    ci: "",
-                    cs: "",
-                    iu: "",
-                    pu: "",
-                    oa: "",
-                    ot: "",
-                    or: "",
-                    ev: "",
-                    v: "",
-                    saak: "",
-                    sask: ""
-                ),
-                config: ""
+            SytelineAPIRest_01 sytelineAPI_ION = new(
+                new SytelineConnection(
+                    APIType: "ION",
+                    Config: "",
+                    CredentialsION: new(
+                        ti: "",
+                        cn: "",
+                        dt: "",
+                        ci: "",
+                        cs: "",
+                        iu: "",
+                        pu: "",
+                        oa: "",
+                        ot: "",
+                        or: "",
+                        ev: "",
+                        v: "",
+                        saak: "",
+                        sask: ""
+                    )
+                )
             );
 
             // INITIALIZE THE RESTv2 API DIRECTLY, USING YOUR REGULAR SYTELINE ACCOUNT CREDENTIALS
 
-            SytelineRestAPI sytelineRestAPIDirect = new(
-                sytelineCredentials: new SytelineAuthManager(
-                    username: "",
-                    password: "",
-                    baseURL: ""
-                ),
-                config: ""
+            SytelineAPIRest_01 sytelineAPI_Direct = new(
+                new SytelineConnection(
+                    APIType: "Direct",
+                    Config: "",
+                    CredentialsDirect: new(
+                        Username: "",
+                        Password: "",
+                        BaseURL: "https://csi10c.erpsl.inforcloudsuite.com"
+                    )
+                )
             );
 
             // LOAD A SAMPLE REQUEST
 
-            LoadCollectionResponse response = sytelineRestAPIThroughION.LoadCollection(
+            APILoadCollectionResponse response_ION = sytelineAPI_ION.LoadCollection(
                 idoName: "SLCustomers",
-                properties: new List<string>() {
+                properties: [
                     "CustNum",
                     "CustSeq",
                     "Name",
@@ -52,11 +61,31 @@
                     "City",
                     "StateCode",
                     "Country"
-                },
+                ],
                 filter: "CustNum = 'C000001'"
             );
 
-            Console.WriteLine($"The request retrieved {response.Items.Count} items.");
+            // LOAD A SAMPLE REQUEST
+
+            APILoadCollectionResponse response_Direct = sytelineAPI_Direct.LoadCollection(
+                idoName: "SLCustomers",
+                properties: [
+                    "CustNum",
+                    "CustSeq",
+                    "Name",
+                    "Addr_1",
+                    "Addr_2",
+                    "Addr_3",
+                    "Addr_4",
+                    "City",
+                    "StateCode",
+                    "Country"
+                ],
+                filter: "CustNum = 'C000001'"
+            );
+
+            Console.WriteLine($"The ION request retrieved {response_ION.Items.Count} items.");
+            Console.WriteLine($"The Direct request retrieved {response_Direct.Items.Count} items.");
             Console.ReadLine();
 
         }
